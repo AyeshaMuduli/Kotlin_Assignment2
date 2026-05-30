66b6340857c770dfae54aef7d350f4519513e689
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-30 15:28:12 +0530

    Square

---
+++ KotlinProgram/Question11.kt
@@ -0,0 +1,9 @@
+// 11. Write a variable val Square: Int-> that holds a lambda function. Use the it keyword to return the square of the input number
+fun main() {
+
+    val square: (Int) -> Int = {
+        it * it
+    }
+
+    println(square(5))
+}
