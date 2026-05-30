d56131b397720523f1fccb83b07bcd3f20de2e6b
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-30 15:11:04 +0530

    Database

---
+++ KotlinProgram/Question8.kt
@@ -0,0 +1,11 @@
+// 8. write an object Database with a fun correct() that prints "Connected to DB"
+object Database {
+
+    fun connect() {
+        println("Connected to DB")
+    }
+}
+
+fun main() {
+    Database.connect()
+}
