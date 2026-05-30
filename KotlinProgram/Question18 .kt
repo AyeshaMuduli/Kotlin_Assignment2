0ec04081a9243addbc38faea3770c5f60bdb5b07
AyeshaMuduli <ece.24beca94@silicon.ac.in>
2026-05-30 15:55:56 +0530

    Weather

---
+++ KotlinProgram/Question18 .kt
@@ -0,0 +1,8 @@
+// 18. Create a suspend function fetchWeather() that simulates fetching weather data by delaying for one second before returning a result.
+fun fetchWeather(): String {
+    return "Rainy"
+}
+
+fun main() {
+    println(fetchWeather())
+}
